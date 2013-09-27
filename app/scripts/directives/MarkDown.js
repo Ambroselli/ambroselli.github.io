2669f67f39639d7867a8ac84c73093c95ce5cb35
hugodias <hugo.victor.dias.teodoro@gmail.com>
2013-09-27 19:11:54 -0300

    Verify if has a link before get the file via ajax

--- app/scripts/directives/MarkDown.js
+++ app/scripts/directives/MarkDown.js
@@ -8,10 +8,12 @@ angular.module('blogApp')
       scope: { link:'@' },
       link: function (scope, element, attrs) {
         attrs.$observe('link',function(link){
+          if(link){
             $http.get('/posts/files/' + link).success(function(response){
               var htmlText = converter.makeHtml(response);
               element.html(htmlText);
             });
+          }
         });
       }
     };
