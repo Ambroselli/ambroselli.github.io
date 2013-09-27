d94d6ed6e2824fa79b3e7c7f077123f8a1d20993
ribeiroevandro <ribeiroevandro@live.com>
2013-09-27 18:45:58 -0300

    correcting titles.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,8 +2,6 @@
 
 angular.module('blogApp')
   .controller('MainCtrl', function ($scope, $window, Posts, Config, $routeParams, progressbar) {
-    $scope.appTitle = "Ambe";
-    $scope.appSubtitle = "AngularJS + Markdown blog engine";
 
     Config.make();
 
@@ -16,11 +14,11 @@ angular.module('blogApp')
               Posts.getBySlug($routeParams.postTitle, function(data){
                 $scope.post = data;
                 progressbar.complete();
-                $window.document.title = $scope.post.title + " | " + $scope.appTitle;
+                $window.document.title = $scope.post.title + " - " + $scope.config.title;
               });
             } else {
 
-              $window.document.title = $scope.appTitle;
+              $window.document.title = $scope.config.title + " - " + $scope.config.description;
 
               Posts.getPosts(function(data){
                 $scope.posts = data;
